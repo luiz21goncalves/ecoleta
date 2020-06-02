77f2ef26c15493c83fcd204148456ac26f3752ee
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-06-02 11:16:11 -0300

    add seeds and static files

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -3,7 +3,8 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('items', table => {
     table.increments('id').primary();
-    table.string('imagem').notNullable();
+    table.string('title').notNullable();
+    table.string('image').notNullable();
   });
 };
 
